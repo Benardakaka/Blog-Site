d25d0818784b7db86ef2d0e7aaaaf8424102afc9
Benardakaka <benardakaka484@gmail.com>
2020-12-13 11:28:05 +0300

    add

--- config.py
+++ config.py
@@ -5,18 +5,25 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = 'bena'
-    SECRET_KEY =os.environ.get('SECRET_KEY')
-    API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://postgres:20302000b@localhost/blog'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
- 
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:20302000b@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-    MAIL_SERVER = 'smtp.googlemail.com'
+     #email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'blog'
+    SENDER_EMAIL = 'benardakaka484@gmail.com'
 
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+   
 class ProdConfig(Config):
     '''
     Production  configuration child class
@@ -24,25 +31,33 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
-    
+    # TESTING = True
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:20302000b@localhost/blog'
 
 class DevConfig(Config):
-
-    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://postgres:20302000b@localhost/blog'
     '''
     Development  configuration child class
 
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:20302000b@localhost/blog'
 
-    
     DEBUG = True
+    ENV = 'development'
     
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig
-    }
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
