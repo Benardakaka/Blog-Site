bce906d42c6b7925d8f6fa74a3680f2fd523ae0b
Benardakaka <benardakaka484@gmail.com>
2020-12-13 11:27:53 +0300

    add

--- manage.py
+++ manage.py
@@ -3,19 +3,26 @@ from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from app.models import User, Blog
 
-app = create_app("development")
+
+#Creating app instance
+app = create_app('development')
 
 manager = Manager(app)
+manager.add_command('server',Server)
 
 migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
-manager.add_command("db", MigrateCommand)
-manager.add_command("server", Server)
-
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Blog=Blog)
+    return dict(app = app,db = db,User = User)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     manager.run()
