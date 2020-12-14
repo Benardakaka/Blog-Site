f7650242d394b6426e90d7038957c9e78e15db0e
Benardakaka <benardakaka484@gmail.com>
2020-12-14 13:14:37 +0300

    add

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from . import main
-from ..models import Blog, User, Comment
-from .forms import BlogForm, CommentForm
+from ..models import Blog, User, Comment,Subscriber
+from .forms import BlogForm, CommentForm,SubscriberForm
 from .. import db
 from ..requests import get_quotes
 
@@ -98,5 +98,23 @@ def view_comments(blog_id):
         return redirect(url_for("main.view_comments", blog_id=blog.id))
 
     return render_template(
-        "comments.html", blog=blog, comments=comments, comment_form=form
-    )
+        "comments.html", blog=blog, comments=comments, comment_form=form)
+
+    @main.route('/subscribe', methods=['GET','POST'])
+        
+    def subscriber():
+        quote = get_quotes()
+        subscriber_form=SubscriberForm()
+        blog = Blog.query.order_by(Blog.date.desc()).all()
+        if subscriber_form.validate_on_submit():
+            subscriber= Subscriber(email=subscriber_form.email.data,name = subscriber_form.name.data)
+            
+            db.session.add(subscriber)
+            db.session.commit()
+
+            mail_message("Welcome to K-Blogs","email/subscriber",subscriber.email,subscriber=subscriber)
+            title= "K-BLOGS"
+            return render_template('index.html',title=title, blog=blog, quote=quote)
+        subscriber = Blog.query.all()
+        blog = Blog.query.all()
+        return render_template('subscribe.html',subscriber=subscriber,subscriber_form=subscriber_form,blog=blog)
